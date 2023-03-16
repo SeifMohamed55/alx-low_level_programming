6a3111cfa4b1358c2920b869244f9ea99b20470b
SeifMohamed55 <seifelden073@gmail.com>
2023-03-16 19:51:57 +0200

    ds

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
  * main - This is a description
+ *
+ * Return: 0
  */
 int main(void)
 {
 
 	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
+	return (0); /*program ends here*/
 
 }
