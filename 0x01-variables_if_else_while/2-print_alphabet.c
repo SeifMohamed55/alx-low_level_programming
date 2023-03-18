5dee6a923427acd3a28af184410db0cfb775f39a
SeifMohamed55 <seifelden073@gmail.com>
2023-03-18 04:04:10 +0200

    dsad

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - describtion
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 122)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar(' ');
+	return (0);
+
+}
