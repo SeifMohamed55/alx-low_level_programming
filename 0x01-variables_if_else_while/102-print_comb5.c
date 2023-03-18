c4de2d38f594035aa090b3db848671c7d5dcb660
SeifMohamed55 <seifelden073@gmail.com>
2023-03-18 05:12:23 +0200

    dasa

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,20 +8,34 @@ int main(void)
 {
 	int x = 0;
 	int y = 0;
+	int z = 0;
+	int q = 0;
 
 	while (x <= 9)
 	{
 		y = 0;
 		while (y <= 9)
 		{
-			putchar(x + '0');
-			putchar(y + '0');
-			if (x == 9)
-				break;
-			putchar(',');
-			putchar(' ');
+			q = 0;
+			while (q <= 9)
+			{
+				z = 0;
+				while (z <= 9)
+				{
+					putchar(x + '0');
+					putchar(y + '0');
+					putchar(' ');
+					putchar(q + '0');
+					putchar(z + '0');
+					if (x == 9 && y == 9 && q == 9 && z == 9)
+						break;
+					putchar(',');
+					putchar(' ');
+					z++;
+				}
+				q++;
+			}
 			y++;
-
 		}
 		x++;
 	}
