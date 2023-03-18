8de32536786c2d392fdc24f2693e911c220168aa
SeifMohamed55 <seifelden073@gmail.com>
2023-03-18 04:15:36 +0200

    dsa

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - describtion
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+
+}
