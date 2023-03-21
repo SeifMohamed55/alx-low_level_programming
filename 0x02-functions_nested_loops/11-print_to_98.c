9f8d0426f9f455bf1530592e2c8b6c1b2777a0f7
SeifMohamed55 <seifelden073@gmail.com>
2023-03-21 12:20:13 +0200

    sda

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * print_to_98 - prints from n to 98
+ * @n: start printing from n
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n--);
+			if (n == 98)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n++);
+			if (n == 98)
+			break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	else
+	{
+		printf("%d", 98);
+	}
+
+}
