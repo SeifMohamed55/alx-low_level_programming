c7faeffb1dc80ce2942937e0a5550b19221b6f4f
SeifMohamed55 <seifelden073@gmail.com>
2023-03-21 10:20:50 +0200

    asdas

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _isalpha - checks if letter is an alphabet
+ * @c : the letter to be checked
+ *
+ * Return: 0 if not a letter and 1 if a letter
+ */
+int _isalpha(int c)
+{
+	int x;
+
+	x = isalpha(c);
+	if (x)
+		return (1);
+	else
+		return (0);
+
+}
