81edf310c37d9a0d0bf512aea9e9ef7744c56ae8
SeifMohamed55 <seifelden073@gmail.com>
2023-03-21 11:20:05 +0200

    dsa

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * print_last_digit - prints the last digit of anumber
  * @x: the number to print its last digit
@@ -10,6 +11,7 @@ int print_last_digit(int x)
 {
 	int y = x % 10;
 
+	_putchar(y + '0');
 	return (y);
 
 }
