135be6cb5fd9151d16f3940a88a1f653cfa30641
SeifMohamed55 <seifelden073@gmail.com>
2023-03-21 11:34:37 +0200

    sda

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,11 +7,12 @@
 void times_table(void)
 {
 	int i, j;
+
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			printf("2%d,", i*j);
+			printf("2%d,", i * j);
 		}
 		putchar('\n');
 	}
