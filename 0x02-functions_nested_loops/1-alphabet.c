9005d87e0f612c077e38e0d43db81077779048ae
SeifMohamed55 <seifelden073@gmail.com>
2023-03-21 09:56:40 +0200

    dsa

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - it prints lower case alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return;
+
+}
