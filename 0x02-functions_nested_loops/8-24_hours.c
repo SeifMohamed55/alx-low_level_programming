674d7974e6a8ce1aa243b4caeb73fecc82060719
SeifMohamed55 <seifelden073@gmail.com>
2023-03-21 11:01:15 +0200

    sdaa

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * jack_bauer - print every minute and hour of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	char x = '0', y = '0', z = '0', w = '0';
+	int l = 0;
+
+	while (x <= '2' )
+	{
+		y = '0';
+		while (y <= '9')
+		{
+			z = '0';
+			while (z <= '5')
+			{	w = '0';
+				while (w <= '9')
+				{
+					if (x == '2' && y == '4')
+						return;
+					printf("%c%c:%c%c\n", x, y, z, w);
+					w++;
+				}
+				z++;
+			}
+			y++;
+		}
+		x++;
+	}
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "8-24_hours.c"
+/**
+ * main - this function output a word
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	jack_bauer();	
+	return (0);
+
+
+
+}
