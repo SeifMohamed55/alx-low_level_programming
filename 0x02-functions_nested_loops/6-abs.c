e3168f1007ce8d7e560aa63d049f370682d74a21
SeifMohamed55 <seifelden073@gmail.com>
2023-03-21 10:32:11 +0200

    sda'

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _abs - prints the absluote value of a number
+ * @n: number to be compared
+ *
+ * Return: x the absloute number of n
+ */
+
+int _abs(int n)
+{
+	int x = abs(n);
+
+	return (x);
+
+}
