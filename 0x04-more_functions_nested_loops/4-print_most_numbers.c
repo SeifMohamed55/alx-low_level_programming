d3e87e8d75579b02b8ff8ebb543ad21131b383c0
SeifMohamed55 <seifelden073@gmail.com>
2023-03-24 09:54:26 +0200

    dsa

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i == 2 || i==4)
+		{
+			i++;
+			continue;
+		}
+		_putchar('0' + i);
+		i++;
+
+	}
+	_putchar('\n');
+
+}
