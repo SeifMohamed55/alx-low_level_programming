88f12f14b1fc30400716df24f92b5af4c0f0a316
SeifMohamed55 <seifelden073@gmail.com>
2023-03-24 10:49:17 +0200

    sda

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - it prints
+ * @n: number of lines
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	while (n--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
