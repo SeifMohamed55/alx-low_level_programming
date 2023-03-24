74052137308031bec0493465c6507b8fa429423c
SeifMohamed55 <seifelden073@gmail.com>
2023-03-24 16:32:42 +0200

    dsa

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - test positive or negative
+ * @i: num to be tested
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+	positive_or_negative(0); 
+	return (0);
+}
