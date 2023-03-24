00dc8717b95d0559aa50aba1e36e94170b61437e
SeifMohamed55 <seifelden073@gmail.com>
2023-03-24 09:21:35 +0200

    dsa

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * _isupper - retuns if the char is upper or not
+ * @c: the char
+ *
+ * Return: true if upper and false otherwise
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (true);
+	else
+		return (false);
+}
