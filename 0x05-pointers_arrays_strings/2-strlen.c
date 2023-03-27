68e5eaee0736e26f4cc108e3529520fa0d01f032
SeifMohamed55 <seifelden073@gmail.com>
2023-03-27 22:14:34 +0200

    Ddas

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - gets length of string
+ * @s: the string
+ *
+ * Return:length of a string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+
+}
+
