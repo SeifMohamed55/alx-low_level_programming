524a5497da99d30002449ed97fc098635a5c9a11
SeifMohamed55 <seifelden073@gmail.com>
2023-03-27 22:20:47 +0200

    da

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts: prints a string to std output
+ * @str: the string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
