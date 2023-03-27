34f5bd26818aa5e26481c88c6ff3bdc5efa352e2
SeifMohamed55 <seifelden073@gmail.com>
2023-03-27 22:31:57 +0200

    das

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - print a sting backwards
+ * @s: the string to be reversed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0, sum = 0;
+
+	while (s[i] != '\0')
+	{
+		sum++;
+		i++;
+	}
+	for (i = sum ; i >= 0 ; i--)
+	{
+		_putchar(a[i]);
+	}
+
+
+}
