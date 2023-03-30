afd378c2cda00c2b5b67e5fe49b2f7bd13b60da0
SeifMohamed55 <seifelden073@gmail.com>
2023-03-31 00:48:30 +0200

    dsa

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,9 +9,9 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i,j;
+	int i, j;
 
-	for (i = 0, j = n-1 ; i < n/2 ; i++, j--)
+	for (i = 0, j = n - 1 ; i < n / 2 ; i++, j--)
 	{
 		int temp = a[i];
 
