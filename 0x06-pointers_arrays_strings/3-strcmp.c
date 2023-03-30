c5d4ef1504f7da4125c0d0876ad5ce8f2dee844c
SeifMohamed55 <seifelden073@gmail.com>
2023-03-31 00:40:53 +0200

    ads

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - copies a string into other string
+ * @src: the src to be copied
+ * @dest: the dest str to be copied into
+ *
+ * Return: char* pointer to dest
+ */
+int _strcmp(char *dest, char *src)
+{
+	return (strcmp(dest, src));
+}
