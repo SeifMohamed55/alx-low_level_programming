8c3db310c047ce24be9925a0fc725a09d682e593
SeifMohamed55 <seifelden073@gmail.com>
2023-03-31 00:29:57 +0200

    das

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat - concatinate two string with at most n bytes from source
+ * @src: the src string
+ * @dest: destinationn string to be concatinated
+ * @n: the number of bytes to be taken at most form
+ *
+ * Return: *dest the destination string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src ,n);
+	return (dest);	
+}
