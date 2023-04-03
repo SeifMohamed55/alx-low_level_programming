559e1fbc332eacff1a25dc1aaccd8cf0ecad5545
SeifMohamed55 <seifelden073@gmail.com>
2023-04-04 00:44:26 +0200

    asd

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - return a pointer to the first occurence
+ * @s: the string
+ * @c: the charachter
+ *
+ * Return: pointer to the first occurence
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
