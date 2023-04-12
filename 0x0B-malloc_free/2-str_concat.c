7cf39a6037bff977ddcc20dc5a92d2576acfaa1f
SeifMohamed55 <seifelden073@gmail.com>
2023-04-12 23:56:59 +0200

    asdds

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, len1 = 0, len2 = 0, j = 0, st = 1 , nd = 1;
+	int i = 0, len1 = 0, len2 = 0, j = 0, st = 1, nd = 1;
 	char *x;
 
 	if (s1 == NULL)
