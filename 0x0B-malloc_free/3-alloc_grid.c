de8c84aa7f6eb4d8126db7ec3dd9bf1f94227ddb
SeifMohamed55 <seifelden073@gmail.com>
2023-04-13 00:06:27 +0200

    sda

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - makes a 2d matrix
+ * @width: no of cols
+ * @heigt: no of rows
+ *
+ * Return: ptr to the matrinx
+ */
+int **alloc_grid(int width, int height)
+{
+	int *x;
+
+	if (width <= 0 || height <=0)
+		return (NULL);
+	x = malloc(sizeof(int) * width * height );
+	if (x == NULL)
+		return (NULL);
+	return (&x);
+}
