d8b639edee4f067e35ad4545fae4302611088587
SeifMohamed55 <seifelden073@gmail.com>
2023-04-12 23:56:20 +0200

    asd

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,18 +9,29 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, len1 = 0, len2 = 0, j = 0;
+	int i = 0, len1 = 0, len2 = 0, j = 0, st = 1 , nd = 1;
 	char *x;
 
-	while (s1[len1] != '\0')
+	if (s1 == NULL)
+		st = 0;
+	if (s2 == NULL)
+		nd = 0;
+	if (st)
 	{
-		len1++;
+		while (s1[len1] != '\0')
+		{
+
+			len1++;
+		}
 	}
-	while (s2[len2] != '\0')
+	if (nd)
 	{
+		while (s2[len2] != '\0')
+		{
+			len2++;
+		}
 		len2++;
 	}
-	len2++;
 	x = malloc(sizeof(char) * (len1 + len2));
 	if (x == NULL)
 		return (NULL);
