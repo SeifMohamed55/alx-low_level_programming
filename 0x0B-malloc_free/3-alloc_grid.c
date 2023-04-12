c431ced0d2bab3f18b18d503cd001ab96cb83bf4
SeifMohamed55 <seifelden073@gmail.com>
2023-04-13 00:13:54 +0200

    sdf

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <=0)
 		return (NULL);
-	x[height] = malloc(sizeof(int[height][width]));
+	x = malloc(sizeof(int) * width * height);
 	if (x == NULL)
 		return (NULL);
 	return (x);
