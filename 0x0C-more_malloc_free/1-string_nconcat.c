8c2ee4804693d33bff4a5576d24e761fdbcefd51
SeifMohamed55 <seifelden073@gmail.com>
2023-04-15 04:33:42 +0200

    sda

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,8 +15,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *new;
 	int t1 = 1, t2 = 1;
 
-	if (new == NULL)
-		return (NULL);
 	if (s1 == NULL)
 	{
 		t1 = 0;
@@ -32,6 +30,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= x2)
 		n = x2;
 	new = malloc((x1 + n + 1) * sizeof(char));
+	if (new == NULL)
+		return (NULL);
 	if (t1)
 	{
 		while (s1[i] != '\0')
