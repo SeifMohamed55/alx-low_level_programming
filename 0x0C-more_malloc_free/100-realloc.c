a3236a81c9a2a9607766bc3db8e379730ee490b3
SeifMohamed55 <seifelden073@gmail.com>
2023-04-15 04:58:01 +0200

    asd

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * min - retuns the minimum
+ * @a:first num
+ * @b: the second num
+ *
+ * Return minimum
+ */
+int min(int a, int b)
+{
+	if (a > b)
+		return (a);
+	else
+		return (b);
+}
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: ptr previously allocated
+ * @old_size: tje old size
+ * @new_size: tje new size
+ *
+ * Return: ptr to the reallocated area
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new
+
+	if(new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		new = malloc(new_size);
+		if(new == NULL)
+			return (NULL);
+		return (new);
+	}
+	if(new_size > old_size)
+	{
+		new = malloc(new_size);
+		if (new == NULL)
+		{
+			free(ptr);
+			return (NULL);
+		}
+		memcpy(new, ptr, old_size);
+		free(ptr);
+	}
+	return (new);
+
+}
