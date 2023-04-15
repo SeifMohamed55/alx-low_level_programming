9a9b5020156674909b58aa17cf2b5d3181e7e1a6
SeifMohamed55 <seifelden073@gmail.com>
2023-04-16 01:01:45 +0200

    das

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number of args
+ * @argc: number of args
+ *
+ * Return: 0
+ */
+int main(int argc)
+{
+	printf("%d", argc);
+	return (0);
+}
