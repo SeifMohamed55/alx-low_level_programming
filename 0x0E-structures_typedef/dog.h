e460ab16b34ac27d873f719ab1b55391a2292d7b
SeifMohamed55 <seifelden073@gmail.com>
2023-04-17 15:32:12 +0200

    das

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
-struct dog {
-
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+/**
+ * dog - struct about dog
+ * @name: array of char
+ * @age: age
+ * owner: array of char
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
