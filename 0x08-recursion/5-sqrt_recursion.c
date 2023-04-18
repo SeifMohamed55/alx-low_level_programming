6add3cb77c3894320ded5c524e9e995b0ccbf90b
SeifMohamed55 <seifelden073@gmail.com>
2023-04-18 14:25:18 +0200

    das

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <math.h>
+/**
+ * _sqrt_recursion - sqrt of a number
+ * @n: the num
+ *
+ * Return: sqrt of number
+ */
+int _sqrt_recursion(int n)
+{
+	return ((int)sqrt((double)n));
+}
