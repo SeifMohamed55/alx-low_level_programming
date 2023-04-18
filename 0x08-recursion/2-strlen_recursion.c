a4d09cc45d93af4ab767036c31bafcd9ece4acba
SeifMohamed55 <seifelden073@gmail.com>
2023-04-18 13:58:44 +0200

    asd

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen_recursion - length of string
+ * @s: the string
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
