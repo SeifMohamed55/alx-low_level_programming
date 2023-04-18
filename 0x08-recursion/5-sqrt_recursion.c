dda101ee3be3d1a194091111ddf01f62d5ee17ba
SeifMohamed55 <seifelden073@gmail.com>
2023-04-18 14:40:47 +0200

    dsa

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,23 @@
 #include "main.h"
 #include <math.h>
 /**
+ * sqr - sqrt
+ * @n: the num
+ * @c: counter
+ *
+ * Return: sqrt , -1
+ */
+int _sqrt(int n , int c)
+{
+	if (c * c == n)
+		return (c);
+	if (c * c > n)
+		return (-1);
+	if (c * c < n)
+		return (_sqrt(n, c+1));
+}
+
+/**
  * _sqrt_recursion - sqrt of a number
  * @n: the num
  *
@@ -8,5 +25,11 @@
  */
 int _sqrt_recursion(int n)
 {
-	return ((int)sqrt((double)n));
+	
+
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (_sqrt(n, 1));
 }
