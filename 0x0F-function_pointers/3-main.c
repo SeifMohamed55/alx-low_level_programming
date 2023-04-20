31160d7c99af3fec8248e060847b16f66bd4f41e
SeifMohamed55 <seifelden073@gmail.com>
2023-04-20 04:09:08 +0200

    asd'

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+/**
+ * main - the application
+ * @argc: number of param
+ * @argv: array of strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][0] != '*' || argv[2][0] != '/' || argv[2][0] != '+' || argv[2][0] != '%' || argv[2][0] != '-')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (atoi(a[3]) == 0 && (a[2][0] == '/' || a[2][0] == '%'))
+	{
+		printf("Error");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+/**
+ * op_add - adds two ints
+ * @a: f
+ * @b: s
+ *
+ * Return: add
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub
+ * @a: f
+ * @b: s
+ *
+ * Return: subtract
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply
+ * @a: f
+ * @b: s
+ *
+ * Return: mul
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide
+ * @a: f
+ * @b: s
+ *
+ * Return: div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod
+ * @a: f
+ * @b: s
+ *
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
