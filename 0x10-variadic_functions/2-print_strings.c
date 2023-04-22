bff23809602b8d26c5c76cc645a83f57519e2dcb
SeifMohamed55 <seifelden073@gmail.com>
2023-04-22 04:03:31 +0200

    asd

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		x = va_arg(list, char*);
 		if (x == NULL)
 			printf("(nil)");
-		printf("%s", x);
+		else
+			printf("%s", x);
 		if (separator == NULL)
 			continue;
 		if (i == n - 1)
