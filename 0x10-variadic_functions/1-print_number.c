e0e8d7e13ecf616542f88c4ca4711518537f3fa0
SeifMohamed55 <seifelden073@gmail.com>
2023-04-22 03:44:32 +0200

    dsa

---
+++ 0x10-variadic_functions/1-print_number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - followed by string
+ * @seperator: the string
+ * @n: number of parameters
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list lis;
+	unsigned int i;
+	int k
+	
+	va_start(lis,n);
+	for (i = 0; i < n ;i++)
+	{
+		k = va_arg(lis, int);
+		printf("%d",k);
+		if (separator == NULL)
+			continue;
+		if(i == n - 1)
+			break;
+		printf("%s", separator);
+	}
+	printf("\n");
+}
