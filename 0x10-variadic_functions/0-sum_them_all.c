e3b4589f663ebfff98121c49f0c60acc7ad1f42f
SeifMohamed55 <seifelden073@gmail.com>
2023-04-22 03:33:46 +0200

    sda

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
-int sum_them_all(const unsigned int n, ....)
+int sum_them_all(const unsigned int n, ...)
 {
 	va_list lis;
 	unsigned int i;
