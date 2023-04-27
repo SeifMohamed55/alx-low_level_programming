0f10e8714a0a4eb17f7785ade4200cf8a5d99745
SeifMohamed55 <seifelden073@gmail.com>
2023-04-27 16:29:03 +0200

    asd

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - number of elements
+ * @h: the list
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *p = h;
+	size_t c = 0;
+
+	if (h == NULL)
+		return (0);
+	while(p)
+	{
+		if (p->str == NULL)
+		{
+			p = p->next;
+			continue;
+		}
+		c++;
+		p = p->next;
+	}
+	return (c);
+}
