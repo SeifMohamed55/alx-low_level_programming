40d61dbd083c17ffb4e704cd409e5b219febb63e
SeifMohamed55 <seifelden073@gmail.com>
2023-04-27 16:49:48 +0200

    asd

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,23 +11,15 @@ size_t print_list(const list_t *h)
 {
 	const list_t *p = h;
 	int i = 0;
-	unsigned int j;
 
 	if (h == NULL)
 		return (0);
-	while (p)
+	while (p->next != NULL )
 	{
 		if (p->str == NULL)
 			printf("[0] (nil)\n");
 		else
-		{
-			printf("[%u] ", p->len);
-			for (j = 0; j < p->len ; j++)
-			{
-				_putchar(p->str[j]);
-			}
-			_putchar('\n');
-		}
+			printf("[%u] %s\n", p->len, p->str);
 		i++;
 		p = p->next;
 	}
