ed6ff42303462851b7c5476402621de5998774a1
SeifMohamed55 <seifelden073@gmail.com>
2023-05-09 04:40:36 +0200

    asddas

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,12 +34,14 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		op = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+		if (op == -1)
+			return (-1);
 		close(op);
 		return (1);
 	}
 	else
 	{
-		op = open(filename, O_CREAT | O_TRUNC, 0600);
+		op = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 		if (op == -1)
 		{
 			close(op);
