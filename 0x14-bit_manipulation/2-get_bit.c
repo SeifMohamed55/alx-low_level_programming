14d985e6dcb5ca1af5a4ef4cf0630187625135cf
SeifMohamed55 <seifelden073@gmail.com>
2023-07-06 19:52:14 +0200

    PUSH

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,16 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * print_binary - prjkfabuba
+ * get_bit - prjkfabuba
  * @n: decimal num
  * @index: index
  *
- * Return: void
+ * Return: 1 if on 0 if off and -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int i;
 
+	if (index > 31)
+		return (-1);
 	i = 1 << (index);
 	if (n & i)
 		return (1);
