59bc5077cc23066c2e16e7d4047342e77013e2dc
SeifMohamed55 <seifelden073@gmail.com>
2023-07-06 21:04:48 +0200

    PUSH

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * set_bit - prjkfabuba
+ * @n: decimal num
+ * @index: index
+ *
+ * Return: 1  and -1 if an error occured
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > 31)
+		return (-1);
+	i = ULONG_MAX - (1 << index);
+	*n = *n & i;
+	return (1);
+}
