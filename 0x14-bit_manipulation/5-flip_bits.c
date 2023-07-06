7cc29364a1066bfd35c0af14c8cd5e138c013b97
SeifMohamed55 <seifelden073@gmail.com>
2023-07-06 22:35:05 +0200

    PUSh

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * flip_bits - prjkfabuba
+ * @n: decimal num
+ * @m: num
+ *
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int i = n ^ m;
+	unsigned long int x = 1 << 30;
+	int c = 0;
+
+	while (x > 0)
+	{
+		if (i & x)
+			c++;
+		x = x >> 1;
+	}
+	return (c);
+}
