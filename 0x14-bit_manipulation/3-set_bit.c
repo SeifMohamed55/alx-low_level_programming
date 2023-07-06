638ede07543ccf2815bfb1098e99b79157aca4fc
SeifMohamed55 <seifelden073@gmail.com>
2023-07-06 20:53:20 +0200

    PUSH

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * get_bit - prjkfabuba
+ * @n: decimal num
+ * @index: index
+ *
+ * Return: 1 if on 0 if off and -1 if an error occured
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > 31)
+		return (-1);
+	i = 1 << (index);
+	*n = *n | i;
+	return (1);
+}
